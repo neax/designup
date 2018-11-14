d61b723e79552c6a8df52ecfb0d12ff62fe12474
Renato Guzman <rguznx@users.noreply.github.com>
2018-11-14 13:07:41 -0500

    Merge branch 'master' into autocomplete

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,22 @@
 <template lang="html">
   <div>
-    <nav>
+    <nav :class="color">
       <span class="nav-logo">
-        <nuxt-link to="/">
-          <font-awesome-icon :icon="icon" /> {{ title }}
-        </nuxt-link>
+        <nuxt-link :to="to"> <font-awesome-icon v-if="icon" :icon="icon" /> {{ title }} </nuxt-link>
       </span>
-      <a v-on:click="responsive = !responsive" class="nav-responsive-button" id="navResponsiveButton"><font-awesome-icon icon="bars" /></a>
 
-      <div class="nav-left" :class="{ 'nav-responsive' : responsive }">
+      <a
+        v-on:click="responsive = !responsive"
+        class="nav-responsive-button"
+        id="navResponsiveButton"
+        ><font-awesome-icon icon="bars"
+      /></a>
+
+      <div class="nav-left" :class="{ 'nav-responsive': responsive }">
         <slot name="left"></slot>
       </div>
 
-      <div class="nav-right" v-if="$slots.right" :class="{ 'nav-responsive' : responsive }">
+      <div class="nav-right" v-if="$slots.right" :class="{ 'nav-responsive': responsive }">
         <slot name="right"></slot>
       </div>
     </nav>
@@ -21,7 +25,13 @@
 
 <script>
 export default {
-  props: ['title', 'icon'],
+  props: {
+    title: { type: String },
+    icon: { type: String },
+    color: { type: String, default: 'primary' },
+    to: { type: String, default: '/' }
+  },
+
   data() {
     return {
       responsive: false
--- src/components/NavDropdown.vue
+++ src/components/NavDropdown.vue
@@ -1,12 +1,20 @@
 <template lang="html">
   <div class="nav-dropdown">
     <a href="javascript:void(0)" class="nav-dropdown-button">
-      {{ title }}
-      <font-awesome-icon icon="angle-down" class="nav-dropdown-icon" v-if="icon" />
+      <template v-if="typeof title === 'string'">
+        {{ title }}
+      </template>
+      <template v-else>
+        <font-awesome-icon
+          :icon="title.icon"
+          v-if="title.icon"
+          :class="{ 'icon-left': title.icon && title.name }"
+        />
+        {{ title.name }}
+      </template>
+      <font-awesome-icon icon="angle-down" class="nav-dropdown-icon-arrow" v-if="icon" />
     </a>
-    <div class="nav-dropdown-content">
-      <slot></slot>
-    </div>
+    <div class="nav-dropdown-content"><slot></slot></div>
   </div>
 </template>
 
@@ -16,3 +24,9 @@ export default {
   props: ['icon', 'title']
 }
 </script>
+
+<style scoped>
+.icon-left {
+  margin-right: calc(var(--spacing) / 4);
+}
+</style>
